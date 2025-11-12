7dbdea9cea724aef9533208e8ad2f3100911b6e5
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2025-11-12 12:39:58 +0300

    add docker and request to service

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,13 +1,40 @@
 import { useSelector } from "react-redux";
 import withAuth from "../../HOC/withAuth";
 import type { RootState } from "../../store";
+import axios from "axios";
+import { useState } from "react";
 
 const HomePage = () => {
   const user = useSelector((state: RootState) => state.user.user);
+
+  const [data, setData] = useState<string | undefined>(undefined);
+
+  const handleClick = () => {
+    setData(undefined);
+    axios
+      .get("https://localhost:4040/weatherforecast")
+      .then((response) => {
+        setData(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Error fetching data:", error);
+      });
+  };
+
   return (
     <>
       <h2>Добро пожаловать на главную страницу!</h2>
       {user && <h3>Вы зарегистрированы под пользователем: {user.username}</h3>}
+      <div>
+        <button onClick={handleClick}>GET WHEATHER</button>
+      </div>
+      {data && (
+        <div>
+          <h1>Погода:</h1>
+          <h2>{data}</h2>
+        </div>
+      )}
     </>
   );
 };
